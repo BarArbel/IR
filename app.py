8704ed3aca42c6cebba0f6cdc45e578df5432784
MiriNu <miryamnudelman@gmail.com>
2019-03-09 23:03:57 +0200

    front

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -12,11 +12,11 @@ POSTGRES = {
 }
 
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+"""app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db.init_app(app)
 
-"""class User(db.Model):
+class User(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
@@ -31,8 +31,43 @@ db.init_app(app)
 
 @app.route('/')
 def index():
+    os.system('add.py')
     return render_template('index.html')
 
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+
+@app.route("/get_file_page", methods=['GET'])
+def get_file_page():
+    if request.method == 'GET':
+        fileName = request.args.getlist('param1')
+        
+        # Get file and print it
+        file = open("files/files_indexed/"+fileName[0]+".txt", "r") 
+        file_content = file.read() 
+        author = file_content.split(',', 1)[0]
+        text = file_content.split("\n",1)[1].replace("\n", "<br>")
+        html_content = "<main> <h1>Literature Search</h1> <h2>"+fileName[0]+" / "+author+"</h2> "+text+" </main>"
+       
+    return render_template("file.html",literaturegohere=html_content)
+
+
+@app.route("/adminZone", methods=['POST'])
+def adminZone():
+    if request.method == 'POST':
+        password = request.form['Enter']
+        if password == "1234":
+            html_content = "<section>"
+            for filename in os.listdir("files/files_indexed/"):
+                if filename.endswith(".txt"): 
+                     # print(os.path.join(directory, filename))
+                    continue
+                else:
+                    continue
+            html_content += "</section>"
+            return render_template("adminZone.html", literaturegohere=html_content)
 
 if __name__ == '__main__':
     app.run()
