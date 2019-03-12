21a5de09d5638925ea687d85b3e48be766e0df9a
bararbel@gmail.com <bararbel@gmail.com>
2019-03-12 18:15:22 +0200

    hidden fix + crawler

--- app.py
+++ app.py
@@ -2,24 +2,26 @@ from flask import Flask, render_template, request
 import os, psycopg2
 from add import add_files
 from search import search_words
-from dbtools import init_schema, file_status,hideshow_file
+from dbtools import init_schema, file_status, hideshow_file
 app = Flask(__name__)
 
 
 app.config['DEBUG'] = False
 
-#Page where a search can be done
+
+# Page where a search can be done
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#Page that shows the results of the query search
+
+# Page that shows the results of the query search
 @app.route("/results", methods=['GET'])
 def results():
     if request.method == 'GET':
         bool_exp = request.args.getlist('search')[0]
         
-        #Search for files
+        # Search for files
         answer = search_words(bool_exp)
         html_content = "<h2>Your query: "+bool_exp+" </h2><br><section>"
         if answer == set():
@@ -31,7 +33,7 @@ def results():
                 file = open("files/files_indexed/"+fileName+".txt", "r") 
                 file_content = file.read() 
                 author = i[2]
-                literature_type =  i[3]
+                literature_type = i[3]
                 text = file_content.split("\n", 5)[2:5]
                 rows = "".join(map(( lambda x: x+'<br>'), text))
                 
@@ -39,7 +41,7 @@ def results():
                 
         html_content += "</section>"
         
-    return render_template("results.html",resultsgohere=html_content)
+    return render_template("results.html", resultsgohere=html_content)
 
 
 @app.route("/get_file_page", methods=['GET'])
@@ -51,7 +53,7 @@ def get_file_page():
         file = open("files/files_indexed/"+fileName[0]+".txt", "r") 
         file_content = file.read() 
         author = file_content.split(',', 1)[0]
-        text = file_content.split("\n",1)[1].replace("\n", "<br>")
+        text = file_content.split("\n", 1)[1].replace("\n", "<br>")
         html_content = "<main> <h1>Literature Search</h1> <h2>"+fileName[0]+" / "+author+"</h2> "+text+" </main>"
        
     return render_template("file.html",literaturegohere=html_content)
@@ -68,21 +70,21 @@ def adminZone():
         if password == "1234":
             html_content = "<section>"
             
-            #Check if directory is empty
+            # Check if directory is empty
             for filename in os.listdir("files/files_indexed/"):
                 if filename.endswith(".txt"): 
                     file = open("files/files_indexed/"+filename, "r") 
                     file_content = file.read() 
                     author = file_content.split(',', 1)[0]
                     
-                    #show if action available equals delete or add
-                    print("this is a file status", file_status(filename,author))
-                    if file_status(filename,author):
+                    # show if action available equals delete or add
+                    print("this is a file status", file_status(filename, author))
+                    if file_status(filename, author):
                         file_command = add_back_file
                     else:
                         file_command = delete_file
                         
-                    html_content+= ('<a class="file"><h2>'+file_command+filename.split('.txt', 1)[0]+'/'+author+'</h2></a>')
+                    html_content += ('<a class="file"><h2>'+file_command+filename.split('.txt', 1)[0]+'/'+author+'</h2></a>')
                     continue
                 else:
                     continue
@@ -90,9 +92,10 @@ def adminZone():
 
             return render_template("adminZone.html", literaturegohere=html_content)
 
+
 # Show or hide files according to an action
-@app.route("/adminAction", methods=['GET'])        
-def adminAction():       
+@app.route("/adminAction", methods=['GET'])
+def adminAction():
     delete_file = "&#128686 &#8998 Delete :"
     add_back_file = "&#128193 &#9547 Add :"
     file_command = ''
@@ -100,26 +103,26 @@ def adminAction():
     
     if request.method == 'GET':
         # Preform action
-        paramFileName= request.args.getlist('param1')[0].split('/', 1)[0]+".txt"
+        paramFileName = request.args.getlist('param1')[0].split('/', 1)[0]+".txt"
         print(paramFileName)
-        paramAuthor= request.args.getlist('param1')[0].split('/', 1)[1]
+        paramAuthor = request.args.getlist('param1')[0].split('/', 1)[1]
         print(paramAuthor)
-        hideshow_file(paramFileName,paramAuthor)
+        hideshow_file(paramFileName, paramAuthor)
         
-        #Check if directory is empty
+        # Check if directory is empty
         for filename in os.listdir("files/files_indexed/"):
             if filename.endswith(".txt"): 
                 file = open("files/files_indexed/"+filename, "r") 
                 file_content = file.read() 
                 author = file_content.split(',', 1)[0]
                 
-                print("this is a file status", file_status(filename,author))
-                if file_status(filename,author):
+                print("this is a file status", file_status(filename, author))
+                if file_status(filename, author):
                     file_command = delete_file
                 else:
                     file_command = add_back_file
                         
-                html_content+= ('<a class="file"><h2>'+file_command+filename.split('.txt', 1)[0]+'/'+author+'</h2></a>')
+                html_content += ('<a class="file"><h2>'+file_command+filename.split('.txt', 1)[0]+'/'+author+'</h2></a>')
                 continue
             else:
                 continue
@@ -127,9 +130,10 @@ def adminAction():
 
     return render_template("adminZone.html", literaturegohere=html_content)
 
+
 if __name__ == '__main__':
     
-    #Initialize connection, update schema if needed and add process files
+    # Initialize connection, update schema if needed and add process files
     con = psycopg2.connect(
             host="localhost",
             database="IR",
@@ -138,7 +142,7 @@ if __name__ == '__main__':
     
     cur = con.cursor()
     
-    #Initialize schema in db
+    # Initialize schema in db
     init_schema(cur)
     con.commit()
     
