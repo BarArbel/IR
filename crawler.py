9eb03cddfd04db174b4ae1b713703d8b084ac968
bararbel@gmail.com <bararbel@gmail.com>
2019-03-13 18:04:58 +0200

    crawler

--- crawler.py
+++ crawler.py
@@ -1,5 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
+
+def english_chars_check(s):
+    try:
+        s.encode(encoding="utf-8").decode("ascii")
+    except UnicodeDecodeError:
+        return False
+    return True
 
 
 def lyrics_crawler(max_pages):
@@ -9,12 +18,31 @@ def lyrics_crawler(max_pages):
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "lxml")
-        s_find = soup.find("h1", {"id": "lyric-title-text"})
-        song_title = s_find.string
-        print(song_title)
-        s_find = soup.find("div", {"class": "artist-meta"})
-        artist = s_find.get("h4").string
-        print(artist)
+        try:
+            s_find = soup.find("h1", {"id": "lyric-title-text"})
+            song_title = s_find.string
+            if english_chars_check(song_title) is False:
+                continue
+            print(song_title)
+            s_find = soup.find("div", {"class": "artist-meta"})
+            artist = s_find.find("h4").string
+            artist = artist.split(",")[0]
+            if english_chars_check(artist) is False:
+                continue
+            print(artist)
+            s_find = soup.find("pre", {"id": "lyric-body-text"})
+            lyrics = re.sub("<[^>]+>", "", str(s_find))
+            if english_chars_check(lyrics) is False:
+                continue
+            print()
+            print(lyrics)
+        except:
+            continue
+
+        filePtr = open("files/files_to_add/"+song_title+".txt", "w")
+        filePtr.write(artist+",Song\n\n")
+        filePtr.write(lyrics)
+
         page += 1
 
 
